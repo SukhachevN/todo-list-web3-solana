19c89e6d7f8ee3b6b22ed1ee048c790fe879c47c
Sukhachev <traed.traed@mail.ru>
2023-03-03 23:24:42 +0700

    feat: create token mint

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -3,5 +3,9 @@ import { PublicKey } from '@solana/web3.js';
 export const emptyArray = new Array(20).fill(0);
 
 export const PROGRAM_ID = new PublicKey(
-    process.env.NEXT_PUBLIC_TODO_LIST_WEB3_PROGRAM_ID ?? ''
+    process.env.NEXT_PUBLIC_PROGRAM_ID ?? ''
+);
+
+export const TOKEN_MINT = new PublicKey(
+    process.env.NEXT_PUBLIC_TOKEN_MINT ?? ''
 );
